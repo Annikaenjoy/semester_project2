8c7ffd648d45f084b6aa8b6dabee3393a74ac660
Annika Engøy <annikaa@live.no>
2023-03-23 21:50:48 +0100

    Character images and character data

--- js/index.js
+++ js/index.js
@@ -4,36 +4,60 @@
 // 529  - Jaime Lannister
 // 238  - Cersei Lannister
 // 1052 - Tyrion Lannister
-// 16  - Margaery Tyrell
-// 2126  - Ygritte
+// 823  - Petyr Baelish
+// 743  - Melisandre
 // 565 - Joffrey Baratheon
 // 148  - Arya Stark
 
 import { baseUrl } from "../settings/api.js";
 const resultsContainer = document.querySelector(".results-container");
 
-const characters = [583, 957, 1303, 529, 238, 1052, 16, 2126, 565, 148];
+// Character numbers to add to baseUrl
+const characters = [583, 957, 1303, 529, 238, 823, 1052, 743, 565, 148];
 
-for (let i = 0; i < characters.length; i++) {
-  let newUrl = baseUrl + characters[i];
+// Loop through characters and fetch. Create new urls and get data
+async function getCharacters() {
+  for (let i = 0; i < characters.length; i++) {
+    let newUrl = baseUrl + characters[i];
 
-  async function getCharacters() {
     try {
       const response = await fetch(newUrl);
       const character = await response.json();
       console.log(character);
 
+      if (character.gender === "Female") {
+        character.gender = "&#9792;";
+      } else {
+        character.gender = "&#9794";
+      }
+
+      const name = character.name;
+
       resultsContainer.innerHTML += `<div class="character-cards">
-        <h4>${character.name}</h4>
-        <p>${character.gender}</p>
+      <h3>${character.name}</h3>
+      <img src="../img/characters/${name}.png" />
+        <p class="gender">Gender: ${character.gender}</p>
         <p>Culture: ${character.culture}</p>
         <p>Born: ${character.born}</p>
+    
         </div>`;
     } catch (error) {
       console.log(error);
     } finally {
     }
   }
+  // Get individual cards with the data and loop over with forEach
+  const characterCards = document.querySelectorAll(".character-cards");
+  console.log(characterCards);
 
-  getCharacters();
+  // onClick event
+  characterCards.forEach((cards) => {
+    cards.addEventListener("click", handleClick);
+  });
+
+  function handleClick(event) {
+    console.log(event);
+  }
 }
+
+getCharacters();
