68cfe66658621c3cea2c066bea4b805bf49ec6bf
Annika Engøy <annikaa@live.no>
2023-04-15 07:21:25 +0200

    Small edits to position and  characters on boardGame

--- js/boardGame.js
+++ js/boardGame.js
@@ -6,11 +6,12 @@ const selectedPlayers = getSelectedPlayers();
 
 selectedPlayers.forEach((selected) => {
   playerContainer.innerHTML += `<div class="player">
+  <p>${selected.name}</p>
     <img src="../assets/img/characters/${selected.name}.png" />
 
     </div>`;
 });
 console.log(selectedPlayers);
 
-let player1 = 0;
-let player2 = 0;
+let player1 = selectedPlayers[0].name;
+let player2 = selectedPlayers[1].name;
